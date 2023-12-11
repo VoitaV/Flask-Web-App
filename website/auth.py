ccc69f1642e1deff1a9bbc1f7fbf54fa980d7919
Valentin Voitovich <v.v.voitovich+git@gmail.com>
2023-12-11 15:27:23 +0300

    Finished user control thing

--- website/auth.py
+++ website/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
@@ -15,16 +16,20 @@ def login():
         if user:
             if check_password_hash(user.password, password):
                 flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
             else:
                 flash('Incorrect password', category='error')
         else:
             flash('Email does not exist.', category='error')
-    return render_template('login.html', boolean = True)
+    return render_template('login.html', user=current_user)
 
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return '<p>Log out</p>'
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 @auth.route('/sign_up', methods = ['GET', 'POST'])
 def sign_up():
@@ -49,5 +54,6 @@ def sign_up():
             db.session.add(new_user)
             db.session.commit()
             flash('Account created!', category='success')
+            login_user(user, remember=True)
             return redirect(url_for('views.home'))
-    return render_template('sign_up.html')
+    return render_template('sign_up.html', user=current_user)
